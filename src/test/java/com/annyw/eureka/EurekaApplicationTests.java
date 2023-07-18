e37cf1adb5c5c3e2f0a5d043ceeff21b0fda1add
Yu (Anny) Wu <anny.wu.925@gmail.com>
2023-07-18 18:16:28 +0800

    Eureka server

---
+++ src/test/java/com/annyw/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annyw.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
